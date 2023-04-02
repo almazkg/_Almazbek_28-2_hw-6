ddc45aa8b1883532ad4926c593ed18c5e9434baf
almazkg <almaztazhimyrza@gmail.com>
2023-04-03 00:25:12 +0600

    hw6

---
+++ venv/Lib/_Almazbek_28-2_hw-6.py
@@ -0,0 +1,40 @@
+def bubble_sort(lst):
+    n = len(lst)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+    return lst
+
+def selection_sort(lst):
+    n = len(lst)
+
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if lst[j] < lst[min_idx]:
+                min_idx = j
+        lst[i], lst[min_idx] = lst[min_idx], lst[i]
+    return lst
+
+lst = [64, 34, 25, 12, 22, 11, 90]
+sorted_lst = bubble_sort(lst)
+print(sorted_lst)
+
+
+def binary_search(arr, target):
+    left, right = 0, len(arr)-1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+lst = [11, 12, 22, 25, 34, 64, 90]
+target = 22
+result = binary_search(lst, target)
+print(result)
